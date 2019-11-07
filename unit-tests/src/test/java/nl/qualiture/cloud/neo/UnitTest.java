48fd7f7eac64b48cee05efeadaa749ae766be0b3
Robin van het Hof <robin.van.het.hof@qualiture.nl>
2019-11-07 14:34:09 +0100

    Initial version

---
+++ unit-tests/src/test/java/nl/qualiture/cloud/neo/UnitTest.java
@@ -0,0 +1,12 @@
+package nl.qualiture.cloud.neo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
